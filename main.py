cbcc3438f62f0c2e9484bf018aa09ae2a20f70f5
NishantRogers <157635543+NishantRogers@users.noreply.github.com>
2025-01-26 15:28:02 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,31 +4,82 @@ import json
 app = Flask(__name__)
 
 global data
-
-# read data from file and store in global variable data
 with open('data.json') as f:
-        data = json.load(f)
+    data = json.load(f)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!' # return 'Hello World' in response
+    return 'Hello, World!'
 
 @app.route('/students')
 def get_students():
-  result = []
-  pref = request.args.get('pref') # get the parameter from url
-  if pref:
-    for student in data: # iterate dataset
-      if student['pref'] == pref: # select only the students with a given meal preference
-        result.append(student) # add match student to the result
-    return jsonify(result) # return filtered set if parameter is supplied
-  return jsonify(data) # return entire dataset if no parameter supplied
+    result = []
+    pref = request.args.get('pref')
+    if pref:
+        for student in data:
+            if student['pref'] == pref:
+                result.append(student)
+        return jsonify(result)
+    return jsonify(data)
 
-# route variables
 @app.route('/students/<id>')
 def get_student(id):
-  for student in data: 
-    if student['id'] == id: # filter out the students without the specified id
-      return jsonify(student)
+    for student in data:
+        if student['id'] == id:
+            return jsonify(student)
 
-app.run(host='0.0.0.0', port=8080)
+@app.route('/stats')
+def get_stats():
+  
+    chicken = 0
+    compSciM = 0
+    compSciS = 0
+    fish = 0
+    infoTechM = 0
+    infoTechS = 0
+    vegetable = 0
+  
+    for student in data:
+        if(student['pref'] == "Chicken"):
+            chicken+=1
+        if(student['pref'] == "Fish"):
+            fish+=1
+        if(student['pref'] == "Vegetable"):
+            vegetable+=1
+        if(student['programme'] == "Computer Science (Major)"):
+            compSciM+=1
+        if(student['programme'] == "Computer Science (Special)"):
+            compSciS+=1
+        if(student['programme'] == "Information Technology (Major)"):
+            infoTechM+=1
+        if(student['programme'] == "Information Technology (Special)"):
+            infoTechS+=1
+
+    result = {
+        "Chicken": chicken,
+        "Computer Science (Major)": compSciM,
+        "Computer Science (Special)": compSciS,
+        "Fish": fish,
+        "Information Technology (Major)": infoTechM,
+        "Information Technology (Special)": infoTechS,
+        "Vegetable": vegetable
+    }
+    return jsonify(result)
+
+@app.route('/add/<a>/<b>')
+def add(a, b):
+    return str(int(a) + int(b))
+
+@app.route('/subtract/<a>/<b>')
+def subtract(a, b):
+    return str(int(a) - int(b))
+
+@app.route('/multiply/<a>/<b>')
+def multiply(a, b):
+    return str(int(a) * int(b))
+
+@app.route('/divide/<a>/<b>')
+def divide(a, b):
+    return str(int(a) / int(b))
+
+app.run(host='0.0.0.0', port=8080, debug=True)
